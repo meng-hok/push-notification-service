ba1a95e9b6549a7c0f34b5a4495515981af25a61
yuos yon <yuosyon@gmail.com>
2020-07-03 14:21:28 +0700

    add new function

--- kosign-push-api/src/main/java/com/kosign/push/mybatis/MybatisService.java
+++ kosign-push-api/src/main/java/com/kosign/push/mybatis/MybatisService.java
@@ -1,6 +1,9 @@
 package com.kosign.push.mybatis;
 
 import com.kosign.push.utils.messages.ApplicationResponse;
+import com.kosign.push.utils.messages.ApplicationResponseById;
+import com.kosign.push.utils.messages.PlatformSettingRespone;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +22,17 @@ public class MybatisService {
         applicationResponses = applicationResponses.stream().map(application -> {
             application.setPlatform(application.getAndroid() + application.getIos() + application.getFcm());
 
+            return application;
+        }).collect(Collectors.toList());
+        return applicationResponses;
+    }
+    public List<ApplicationResponseById> getActiveAppsByAppId(String userId, String appId){
+        List<ApplicationResponseById> applicationResponses =  myBatisRepository.findActiveByAppId(userId,appId);
+        List<PlatformSettingRespone> platformSettingRespones = myBatisRepository.findPlatformrByAppId(appId);
+
+        applicationResponses = applicationResponses.stream().map(application -> {
+            application.setPlatform(application.getAndroid() + application.getIos() + application.getFcm());
+            application.setPlatRec(platformSettingRespones);
             return application;
         }).collect(Collectors.toList());
         return applicationResponses;
