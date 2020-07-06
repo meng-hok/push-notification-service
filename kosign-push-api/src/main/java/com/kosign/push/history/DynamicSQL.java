0413201f9837c81d066d28e534e846e049918154
Metra <metrayim@gmail.com>
2020-07-06 08:11:11 +0700

    add end point getAllHistory and by id

---
+++ kosign-push-api/src/main/java/com/kosign/push/history/DynamicSQL.java
@@ -0,0 +1,20 @@
+package com.kosign.push.history;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.annotations.Param;
+
+public class DynamicSQL {
+
+    public String getSQL(@Param("startDate") String startDate ,@Param("endDate") String endDate,@Param("msgTitle") String msgTitle)
+    {
+        String sql="SELECT  ph.id,ph.reciever_id,ph.title,ph.message,ph.to_platform,ph.status,ph.response_msg,ph.created_at,ph.count FROM  ps_history ph \n"+
+        "WHERE ph.created_at >= CONCAT(#{startDate},' 00:00:00')::TIMESTAMP AND \n"+
+        " ph.created_at < CONCAT(#{endDate},' 00:00:00'):: TIMESTAMP";
+        if(StringUtils.isNotBlank(msgTitle))
+        {
+            sql += " AND LOWER(ph.title) LIKE '%' || LOWER(#{msgTitle}) || '%'";
+        }
+        return sql;
+    }
+    
+}
