c218160bbe7121811229b7e1b3e91d50a31189c7
Neng Channa <sjdecode@gmail.com>
2020-07-10 23:54:34 +0700

    update app module - HTTP Response

--- kosign-push-api/src/main/java/com/kosign/push/apps/AppController.java
+++ kosign-push-api/src/main/java/com/kosign/push/apps/AppController.java
@@ -13,12 +13,13 @@
 
 package com.kosign.push.apps;
 
-import com.kosign.push.publics.SuperController;
 import com.kosign.push.apps.dto.RequestAppList;
 import com.kosign.push.apps.dto.RequestAppUpdate;
 import com.kosign.push.apps.dto.RequestCreateApp;
 import com.kosign.push.apps.dto.ResponseListAppById;
 import com.kosign.push.apps.dto.ResponseListApp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.Api;
@@ -26,15 +27,17 @@ import java.util.List;
 import com.kosign.push.users.UserDetail;
 import com.kosign.push.utils.GlobalMethod;
 import com.kosign.push.utils.messages.Response;
-import com.kosign.push.utils.enums.ResponseEnum;
 import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "Applications")
 @PreAuthorize("#oauth2.hasScope('READ')")
 @RestController
 @RequestMapping("/api/v1")
-public class AppController extends SuperController 
+public class AppController 
 {
+	@Autowired
+    public AppService appService;
+	
     @GetMapping("/applications")
     public Object findAllApplications
     (
@@ -44,15 +47,15 @@ public class AppController extends SuperController
         UserDetail userDetail = GlobalMethod.getUserCredential();
         if(userDetail == null)
         {
-            return Response.getResponseBody(ResponseEnum.Message.FAIL, "User Id Not Found", false);
+            return Response.setResponseEntity(HttpStatus.BAD_REQUEST);
         }
         else
         { 
-        	RequestAppList request = new RequestAppList();
-        	request.setName(name);
+        	RequestAppList reqData = new RequestAppList();
+        	reqData.setName(name);
         	
-            List<ResponseListApp> applications = appService.findAllApplications(userDetail.getId(),request);
-            return Response.getResponseBody(ResponseEnum.Message.SUCCESS, applications, true);
+            List<ResponseListApp> respData = appService.findAllApplications(userDetail.getId(),reqData);
+            return Response.setResponseEntity(HttpStatus.OK, respData);
         }
     }
     
@@ -62,52 +65,52 @@ public class AppController extends SuperController
         UserDetail userDetail = GlobalMethod.getUserCredential();
         if(userDetail == null)
         {
-            return Response.getResponseBody(ResponseEnum.Message.FAIL, "User Id Not Found", false);
+        	return Response.setResponseEntity(HttpStatus.BAD_REQUEST);
         }
         else
         { 
-            List<ResponseListAppById> applications = appService.getActiveAppsByAppId(userDetail.getId(),id);
-            return Response.getResponseBody(ResponseEnum.Message.SUCCESS, applications, true);
+            List<ResponseListAppById> respData = appService.getActiveAppsByAppId(userDetail.getId(),id);
+            return Response.setResponseEntity(HttpStatus.OK, respData);
         }
     }
 
     @PostMapping("/applications")
-    public Object createApplication(@RequestBody RequestCreateApp request)
+    public Object createApplication(@RequestBody RequestCreateApp reqData)
     {
         try 
         {
-            if(appService.getAppByNameAndUserId(request.getName()) != null)
+            if(appService.getAppByNameAndUserId(reqData.getName()) != null)
             {
-                throw new Exception("This application already existed");
+            	return Response.setResponseEntity(HttpStatus.CONFLICT);
             }
 
             AppEntity app = new AppEntity();
-            app.setName(request.getName());
+            app.setName(reqData.getName());
             
-            AppEntity responseApp =  appService.save(app);
+            AppEntity respData =  appService.save(app);
             
-            if(responseApp==null )
+            if(respData==null )
             { 
-                throw new Exception("Failed to create application");
+            	return Response.setResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
             }
             
-            return Response.getSuccessResponseNonDataBody(ResponseEnum.Message.SUCCESS);
+            return Response.setResponseEntity(HttpStatus.OK);
         } 
         catch (Exception e) 
         {
             System.out.println(e.getLocalizedMessage());
-            return  Response.getFailResponseNonDataBody(e.getLocalizedMessage());
+            return Response.setResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
       
     }
 
     @PutMapping("/applications/{id}")
-    public Object updateApplicationById(@RequestBody RequestAppUpdate request, @PathVariable("id") String id) throws Exception
+    public Object updateApplicationById(@RequestBody RequestAppUpdate reqData, @PathVariable("id") String id) throws Exception
     {
-        Boolean update = appService.updateApplication(id, request.getName());
+        Boolean update = appService.updateApplication(id, reqData.getName());
         return update ?
-        		Response.getSuccessResponseNonDataBody(ResponseEnum.Message.SUCCESS) :
-        		Response.getFailResponseNonDataBody(ResponseEnum.Message.FAIL);
+        		Response.setResponseEntity(HttpStatus.OK):
+        		Response.setResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @DeleteMapping("/applications/{id}")
@@ -115,7 +118,7 @@ public class AppController extends SuperController
     {
     	Boolean update = appService.disableApplication(id);
         return update ?
-        		Response.getSuccessResponseNonDataBody(ResponseEnum.Message.SUCCESS) :
-        		Response.getFailResponseNonDataBody(ResponseEnum.Message.FAIL);
+        		Response.setResponseEntity(HttpStatus.OK):
+            	Response.setResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- kosign-push-api/src/main/java/com/kosign/push/utils/messages/Response.java
+++ kosign-push-api/src/main/java/com/kosign/push/utils/messages/Response.java
@@ -3,9 +3,29 @@ package com.kosign.push.utils.messages;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+
 public class Response 
 {
-    public static Map<String, Object> getResponseBody(String msg, Object data,Boolean status ) 
+	public static Map<String, Object> setResponseEntity(HttpStatus status, Object respData) 
+    {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status" , status.value());
+        response.put("message", status        );
+        
+        if(respData == null) return response;
+        
+        response.put("data"   , respData      );
+        
+        return response;
+    }
+	
+	public static Map<String, Object> setResponseEntity(HttpStatus status) 
+    {
+       return setResponseEntity(status, null);
+    }
+	
+	public static Map<String, Object> getResponseBody(String msg, Object data,Boolean status ) 
     {
         Map<String, Object> response = new HashMap<>();
         response.put("status",status);
