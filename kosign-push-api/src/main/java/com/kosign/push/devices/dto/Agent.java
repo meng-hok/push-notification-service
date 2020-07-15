6a2e45c3d4084b744ff7f1e6ef3685956a744caf
Neng Channa <sjdecode@gmail.com>
2020-07-15 18:01:46 +0700

    change sql

--- kosign-push-api/src/main/java/com/kosign/push/devices/dto/Agent.java
+++ kosign-push-api/src/main/java/com/kosign/push/devices/dto/Agent.java
@@ -1,4 +1,6 @@
 package com.kosign.push.devices.dto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
@@ -7,22 +9,12 @@ import lombok.ToString;
 @Data
 @ToString
 //@Entity
-public class Agent extends AgentBody {
-
-    // public String id;
-   
-
+public class Agent extends AgentBody 
+{
     public String authorized_key;
-  
     public String pfilename;
     public String team_id;
     public String file_key;
     public String bundle_id;
-
-
-
-   
-  
-
     public Agent(){}
 }
